3b932b196bd39a4dccbadf8efa2a53fb4338af14
FilipDj91 <137201205+FilipDj91@users.noreply.github.com>
2023-09-22 18:27:15 +0200

    arays

--- 09 - Arrays/Examples/script.js
+++ 09 - Arrays/Examples/script.js
@@ -1,23 +1,21 @@
-var words = ["hello", "world", "welcome"];
+var words = ['hello', 'world', 'welcome'];
+var array2 = new Array('eat', 'sleep');
+var mylist = [];
 
-var array2 = new Array("eat", "sleep");
+var numberArray = [2, 4, 6, 8];
 
-var mylist = [2, 4, 6, 8];
+var newData = ['work', 'exercise', 1, true, {}];
 
-var newData = ["work", "exercise", 1, true, {}];
-
-var myArray = ["h", "e", "l", "l", "o"];
+var myArray = ['h', 'e', 'l', 'l', 'o'];
 
 console.log(myArray[0]);
-
 console.log(myArray[1]);
 
-var dailyActivities = ["eat", "sleep"];
-
-dailyActivities.push("exercise");
+var dailyActivities = ['eat', 'sleep'];
+dailyActivities.push('exercise');
 console.log(dailyActivities);
 
-dailyActivities.unshift("work");
+dailyActivities.unshift('work');
 dailyActivities.pop();
 console.log(dailyActivities);
 
@@ -25,6 +23,7 @@ console.log(dailyActivities.length);
 var myFancyNewArray = [];
 
 for (var i = 0; i < dailyActivities.length; i++) {
-  myFancyNewArray.push(dailyActivities[i]);
+    myFancyNewArray.push(dailyActivities[i])
 }
+
 console.log(myFancyNewArray);
--- 09 - Arrays/Task/script.js
+++ 09 - Arrays/Task/script.js
@@ -1,1 +1,15 @@
-var names = ['John', 'Jane', 'Bob',, 'Mike'];
+var names = ['John', 'Jane', 'Bob', , 'Mike'];
+
+//
+
+for (i = 0; 1 < names.length; i++) {
+    console.log(names[i]);
+}
+
+//
+
+names.push("Filip");
+console.log(names[i]);
+
+//
+
